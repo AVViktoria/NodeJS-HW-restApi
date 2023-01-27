32ffdd1c8ea75a7377ed28e293032315c40e3ba9
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-01-26 20:15:47 -0800

    Update contact.js

--- src/schemas/contact.js
+++ src/schemas/contact.js
@@ -1,13 +1,13 @@
 const Joi = require('joi');
-// const numberPattern =
+// const phonePattern =
 //   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
-
+//  or /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/
 
 const contactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().pattern(/^[a-zA-Z]+\s?[a-zA-Z]+$/).min(3).max(30).required(),
   email: Joi.string().email({ minDomainSegments: 2}),
-  phone: Joi.string().min(9).max(11).required()
-  // pattern(numberPattern)
+  phone: Joi.string().pattern(/^[0-9]+$/).min(9).max(11).required()
+  // pattern(phonePattern)
   ,
 });
 
